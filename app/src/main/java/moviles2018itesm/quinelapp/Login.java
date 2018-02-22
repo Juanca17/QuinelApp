2b364798195ff292528356718601aa08baf0ce63
Alejandro Güereca Valdivia <ale_guereca_valdivia@hotmail.com>
2018-02-22 16:03:21 -0600

    Login Implemented, missing change of activity after login and possible registration

--- app/src/main/java/moviles2018itesm/quinelapp/Login.java
+++ app/src/main/java/moviles2018itesm/quinelapp/Login.java
@@ -69,6 +69,7 @@ public class Login extends AppCompatActivity {
     }
 
     public void signIn(View v){
+        Log.d(TAG, usernameField.getText().toString() + " " + passwordField.getText().toString());
         mAuth.signInWithEmailAndPassword(usernameField.getText().toString(), passwordField.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -78,18 +79,30 @@ public class Login extends AppCompatActivity {
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
                         } else {
+                            // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(Login.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
 
                     }
                 });
 
+    }
+
+    private void updateUI(FirebaseUser user){
+        if (user != null){
+
+        } else {
+
+        }
     }
 
 }
