e5b351e9a5745fd5e1a050ce8fc1740fd7bd95d3
Alejandro Güereca Valdivia <ale_guereca_valdivia@hotmail.com>
2018-02-23 15:01:14 -0600

    First parcial semi working

---
+++ app/src/main/java/moviles2018itesm/quinelapp/NewsObject.java
@@ -0,0 +1,11 @@
+package moviles2018itesm.quinelapp;
+
+/**
+ * Created by drag on 23/02/18.
+ */
+
+public class NewsObject {
+    public String title;
+    public String description;
+    public String link;
+}
